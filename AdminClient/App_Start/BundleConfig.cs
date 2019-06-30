e8814f1f714d8285816aa33ead4c8e4399d092b5
Praveen Narsipur <praveen.kumar.np@gmail.com>
2019-06-30 17:48:52 +1000

    front end finetuning

--- AdminClient/App_Start/BundleConfig.cs
+++ AdminClient/App_Start/BundleConfig.cs
@@ -20,12 +20,10 @@ namespace AdminClient
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(            
+                      "~/Content/Site.css"));
         }
     }
 }
